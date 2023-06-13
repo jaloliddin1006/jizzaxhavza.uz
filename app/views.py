6258ab792c14c1bdc9a045480c8cb15d9bfdfc3a
JaloliddinRuzikulov <ruzikulov@tuit.uz>
2023-06-13 18:18:54 +0500

    some update front end

--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from navbar.models import NavbarCategory
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    navbar = NavbarCategory.objects.filter(status='navbar')
+    navbar = [i for i in navbar]
+    print(navbar)
+    return render(request, 'index.html', context={'navbar': navbar})
