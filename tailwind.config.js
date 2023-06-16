8acc7d555edf18af19d96f200528514df1e4521f
husanIbragimov <xasanboyibragimov3@gmail.com>
2023-06-16 04:10:17 +0000

    front end bug fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui"), require('flowbite/plugin')],
   daisyui: {
-    themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+    themes: false, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
     darkTheme: "dark", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
